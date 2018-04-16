3d4699e1de410981f4b7c16595c2116f729254b8
Tom Parker <thpr@yahoo.com>
2018-04-15 20:48:03 -0400

    Docs and Cleanup

--- PCGen-base/code/src/test/pcgen/base/proxy/StagingInfoFactoryTest.java
+++ PCGen-base/code/src/test/pcgen/base/proxy/StagingInfoFactoryTest.java
@@ -1,4 +1,22 @@
+/*
+ * Copyright (c) 2018 Tom Parker <thpr@users.sourceforge.net>
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 package pcgen.base.proxy;
+
 import static org.junit.Assert.fail;
 
 import org.junit.Before;
@@ -9,7 +27,10 @@ import pcgen.base.proxy.ListProcessor;
 import pcgen.base.proxy.MapProcessor;
 import pcgen.base.proxy.StagingInfoFactory;
 
-public class StagingProxyFactoryTest
+/**
+ * Test the StagingInfoFactory class
+ */
+public class StagingInfoFactoryTest
 {
 
 	private StagingInfoFactory factory = new StagingInfoFactory();
